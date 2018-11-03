fd6f7a7c4e96b8e198def0c2196aba7346afe8b0
Mark Bryars <github@darkskiez.co.uk>
2018-11-04 00:26:39 +0100

    Restructure methods/packages

--- main.go
+++ main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"bufio"
 	"context"
-	"crypto/aes"
-	"crypto/sha256"
 	"flag"
 	"fmt"
 	"io/ioutil"
@@ -14,6 +12,7 @@ import (
 
 	"golang.org/x/crypto/ssh/terminal"
 
+	"github.com/darkskiez/u2f-luks/keydb"
 	"github.com/darkskiez/u2f-luks/u2fluks"
 	"github.com/darkskiez/u2fhost"
 )
@@ -34,7 +33,7 @@ func init() {
 	flag.BoolVar(&enrollkey, "enroll", false, "Enroll a key")
 }
 
-var savedAuthorisedKeys u2fluks.AuthorisedKeys
+var keys keydb.AuthorisedKeys
 
 var oldState *terminal.State
 
@@ -75,8 +74,8 @@ func promptPassword(prompt string) (string, error) {
 	return string(password), err
 }
 
-func enrollNewKey(ctx context.Context, app u2fhost.Client) {
-	fmt.Fprintf(os.Stderr, "Insert or tap key")
+func enroll(ctx context.Context, app u2fhost.Client) {
+	fmt.Fprintf(os.Stderr, "Insert or tap key\n")
 	ak, k, err := u2fluks.Enroll(ctx, app)
 	if err != nil {
 		log.Fatal(err)
@@ -87,32 +86,29 @@ func enrollNewKey(ctx context.Context, app u2fhost.Client) {
 		log.Fatal(err)
 	}
 	if pwd != "" {
-		log.Printf("Encrypting keydata")
-		h := sha256.Sum256([]byte(pwd))
-		cipher, err := aes.NewCipher(h[:])
+		ak, err = ak.Encrypt(pwd)
 		if err != nil {
 			log.Fatal(err)
 		}
-		cipher.Encrypt(ak.U2FKeyHandle, ak.U2FKeyHandle)
 	}
 	if keyfile != "" {
-		if err := u2fluks.AppendKeyfile(keyfile, *ak); err != nil {
+		if err := ak.AppendKeyfile(keyfile); err != nil {
 			log.Fatal(err)
 		}
 	}
 	fmt.Printf("%x", k)
 }
 
-func AuthoriseWithToken(ctx context.Context, app u2fhost.Client) {
+func authorise(ctx context.Context, app u2fhost.Client) {
 	backupTerminalState()
 	c := make(chan string)
 	cctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 	authfunc := func() {
-		if len(savedAuthorisedKeys) == 0 {
+		if len(keys) == 0 {
 			return
 		}
-		pwd, err := u2fluks.Authorize(cctx, app, savedAuthorisedKeys)
+		pwd, err := u2fluks.Authorize(cctx, app, keys)
 		if err != nil {
 			log.Printf("Authorise failed: %v", err)
 		} else {
@@ -129,7 +125,7 @@ func AuthoriseWithToken(ctx context.Context, app u2fhost.Client) {
 			}
 			fmt.Fprintf(os.Stderr, "\nInsert or tap U2F key (enter to cancel)\n")
 
-			savedAuthorisedKeys, err = u2fluks.DecryptKeyHandles(pwd, savedAuthorisedKeys)
+			keys, err = keys.Decrypt(pwd)
 			if err != nil {
 				// fallback on returned typed password by itself
 				c <- pwd
@@ -146,7 +142,10 @@ func AuthoriseWithToken(ctx context.Context, app u2fhost.Client) {
 			c <- pwd
 		}()
 	}
-	fmt.Print(<-c)
+
+	key := <-c
+	fmt.Fprintf(os.Stderr, "\n")
+	fmt.Print(key)
 	restoreTerminalState()
 }
 
@@ -168,21 +167,21 @@ func main() {
 	if keyfile != "" {
 		log.Printf("Using keyfile: %v", keyfile)
 		var err error
-		savedAuthorisedKeys, err = u2fluks.LoadKeyfile(keyfile)
+		keys, err = keydb.LoadKeyfile(keyfile)
 		if err != nil {
 			log.Printf("Error loading keys: %v", err)
 		}
-		log.Printf("Loaded %d keys", len(savedAuthorisedKeys))
+		log.Printf("Loaded %d keys", len(keys))
 	}
 
 	ctx := context.Background()
 	app := u2fhost.NewClient(u2fFacet)
 
 	if enrollkey {
-		enrollNewKey(ctx, app)
-	} else if len(savedAuthorisedKeys) == 0 && !tty {
+		enroll(ctx, app)
+	} else if len(keys) == 0 && !tty {
 		log.Fatalf("No keys to authenticate, prompt disabled (enroll some U2F keys)")
 	} else {
-		AuthoriseWithToken(ctx, app)
+		authorise(ctx, app)
 	}
 }
--- u2fluks/u2fluks.go
+++ u2fluks/u2fluks.go
@@ -1,71 +1,40 @@
 package u2fluks
 
 import (
-	"bufio"
 	"bytes"
 	"context"
-	"crypto/aes"
 	"crypto/sha256"
 	"encoding/binary"
-	"encoding/hex"
 	"errors"
 	"fmt"
-	"log"
-	"os"
-	"strings"
 
 	"crypto/elliptic"
 
 	"github.com/darkskiez/eckr"
+	"github.com/darkskiez/u2f-luks/keydb"
 	"github.com/darkskiez/u2fhost"
 )
 
-type AuthorisedKey struct {
-	U2FKeyHandle, PublicKeyHash []byte
-}
-
-// KeyHandler interface
-func (a AuthorisedKey) KeyHandle() u2fhost.KeyHandle {
-	return a.U2FKeyHandle
-}
-
-type AuthorisedKeys []AuthorisedKey
-
-func (aks AuthorisedKeys) KeyHandlers() []u2fhost.KeyHandler {
-	khs := make([]u2fhost.KeyHandler, len(aks))
-	for i, v := range aks {
-		khs[i] = v
-	}
-	return khs
-}
-
-var savedAuthorisedKeys AuthorisedKeys
-
-func Enroll(ctx context.Context, app u2fhost.Client) (*AuthorisedKey, []byte, error) {
+func Enroll(ctx context.Context, app u2fhost.Client) (keydb.AuthorisedKey, []byte, error) {
 	res, err := app.Register(ctx)
-
 	if err != nil {
-		return nil, nil, err
+		return keydb.AuthorisedKey{}, nil, err
 	}
 
 	pubKeyX := res.PublicKey[1:33]
 	pubKeyY := res.PublicKey[33:65]
 
-	//log.Printf("K  %x %x", pubKeyX, pubKeyY)
-	//log.Printf("KH %x", res.KeyHandle)
-
 	// smaller hash output or both keyparts?
 	ksum := sha256.Sum256(pubKeyX)
-	//log.Printf("KS %x", ksum)
 
-	ak := &AuthorisedKey{
+	ak := keydb.AuthorisedKey{
 		U2FKeyHandle:  res.KeyHandle,
 		PublicKeyHash: ksum[:],
 	}
 	return ak, pubKeyY, nil
 }
 
-func Authorize(ctx context.Context, app u2fhost.Client, aks AuthorisedKeys) (string, error) {
+func Authorize(ctx context.Context, app u2fhost.Client, aks keydb.AuthorisedKeys) (string, error) {
 	res, err := app.Authenticate(ctx, aks.KeyHandlers())
 	if err != nil {
 		return "", err
@@ -100,75 +69,4 @@ func Authorize(ctx context.Context, app u2fhost.Client, aks AuthorisedKeys) (str
 	}
 
 	return "", errors.New("Did not match any keys")
-}
-
-func LoadKeyfile(filename string) (AuthorisedKeys, error) {
-	aks := make([]AuthorisedKey, 0, 10)
-
-	file, err := os.Open(filename)
-	if err != nil {
-		return nil, err
-	}
-	defer file.Close() // nolint: errcheck
-
-	scanner := bufio.NewScanner(file)
-	i := 0
-	for scanner.Scan() {
-		i++
-		parts := strings.Split(scanner.Text(), " ")
-		if len(parts) < 2 {
-			log.Printf("failed to parse: %v (line %v)", err, i)
-			continue
-		}
-		kh, err := hex.DecodeString(parts[0])
-		if err != nil {
-			log.Printf("failed to decode keyhandle %v (line %v)", err, i)
-			continue
-		}
-		pkh, err := hex.DecodeString(parts[1])
-		if err != nil {
-			log.Printf("failed to decode keyhash %v (line %v)", err, i)
-			continue
-		}
-		if len(pkh) != 32 {
-			log.Fatalf("keyhash has wrong length %v (expected: 32) (line %v)", len(pkh), i)
-			continue
-		}
-
-		aks = append(aks, AuthorisedKey{
-			U2FKeyHandle:  kh,
-			PublicKeyHash: pkh,
-		})
-	}
-	if err := file.Close(); err != nil {
-		return nil, err
-	}
-	return aks, scanner.Err()
-}
-
-func AppendKeyfile(filename string, a AuthorisedKey) error {
-	w, err := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
-	if err != nil {
-		return err
-	}
-	defer w.Close() // nolint: errcheck
-	_, err = fmt.Fprintf(w, "%x %x\n", a.U2FKeyHandle, a.PublicKeyHash)
-	if err != nil {
-		return err
-	}
-	return w.Close()
-}
-
-func DecryptKeyHandles(password string, aks AuthorisedKeys) (AuthorisedKeys, error) {
-	ret := aks
-	h := sha256.Sum256([]byte(password))
-	cipher, err := aes.NewCipher(h[:])
-	if err != nil {
-		return nil, err
-	}
-	for _, a := range aks {
-		cipher.Decrypt(a.U2FKeyHandle, a.U2FKeyHandle)
-		ret = append(ret, a)
-	}
-	return ret, nil
 }
