25ab3b8cbd203867158c12e23e0e83a0b91c8e08
Mark Bryars <github@darkskiez.co.uk>
2018-11-27 00:58:10 +0100

    Fix keydb tests after format and hash function change

--- keydb/keydb_test.go
+++ keydb/keydb_test.go
@@ -2,7 +2,7 @@ package keydb
 
 import "testing"
 
-var src = "406d4db369985ee0d639f8b441f471f987c51a48674e002b431e3be509266c0bc2d720586907042aba2a45074d7afa098514a58b9cd05fc5aa0268d0c8af57a6 37c58c5aaafd6a5cadf69370b86ce9004c089e0dcb29107c14f48df1ca6c375f"
+var src = "g+wZ3r/Owj/lfSwYSOSHISKNqblwOQ+2hFBFa5xAuowq+gGZVRQ42OiL01CuiPxPUlCh8+hkJqL9ChgjCRkUng== Iez6sHVqePFcSNCmpL1LIzxPzxXERhDW+FKzb1jkFv/9HWhDBgURucWHEtb/XM5rKTsRgepCdN3egcUKRCwxTw=="
 
 func TestSerialization(t *testing.T) {
 	ak, err := DecodeString(src)
