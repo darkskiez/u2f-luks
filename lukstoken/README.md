f006cae80ab699fd62d9f1c0db6a562c96f7868e
Mark Bryars <github@darkskiez.co.uk>
2021-09-04 22:22:25 +0200

    First working prototype

--- lukstoken/README.md
+++ lukstoken/README.md
@@ -1,4 +1,8 @@
+
 fallocate -l 100M testcontainer
 cryptsetup luksFormat testcontainer 
 cryptsetup token import --json-file=testfile.json  testcontainer 
+
+make
+cp libcrypt-token-fido1.so /lib/x86_64-linux-gnu/cryptsetup/libcryptsetup-token-fido1.so
 
--- lukstoken/main.go
+++ lukstoken/main.go
@@ -7,11 +7,18 @@ package main
 // #include <libcryptsetup.h>
 import "C"
 import (
+	"context"
 	"encoding/json"
 	"fmt"
+	"log"
+
+	"github.com/darkskiez/u2f-luks/keydb"
+	"github.com/darkskiez/u2f-luks/u2fluks"
+	"github.com/darkskiez/u2fhost"
 )
 
 var ver = C.CString("0.1")
+var u2fFacet = "u2fkeystore://"
 
 //export cryptsetup_token_open_pin
 func cryptsetup_token_open_pin(cd *C.struct_crypt_device, token C.int, pin *C.char, pinSize C.size_t, password **C.char, password_len *C.size_t, usrptr *C.char) C.int {
@@ -35,10 +42,11 @@ func cryptsetup_token_version() *C.char {
  *       not present in the system
  */
 type TokenConfig struct {
-	Tokentype string `json:"type"`
-	Keyslots  []string
-	Keyhandle string
-	Keyhash   string
+	TokenType string   `json:"type"` // must be fido1
+	KeySlots  []string // which slot this token decrypts
+	KeyHandle string   // keyhandle that identifies token
+	KeyHash   string   // hash of key this decodes
+	IDHandle  string   // keyhandle for identification of token in 2FA mode
 }
 
 //export cryptsetup_token_open
@@ -49,36 +57,103 @@ func cryptsetup_token_open(cd *C.struct_crypt_device, token C.int, password **C.
 
 	/* libcryptsetup API call */
 	cerr := C.crypt_token_json_get(cd, token, &cjson)
-	fmt.Printf("     cerr:%v\n", cerr)
-	if cerr != 0 {
-		return -1
+	if cerr < 0 {
+		fmt.Printf("     cerr:%v\n", cerr)
+		return -cerr
 	}
 	fmt.Printf("     json:%v\n", C.GoString(cjson))
 	var config TokenConfig
 	if err := json.Unmarshal([]byte(C.GoString(cjson)), &config); err != nil {
 		fmt.Printf("err:%v", err)
-		return -2
+		return -C.EINVAL
 	}
-	return -C.ENOANO
 	fmt.Printf(" decoded: %#v\n", config)
-	pwd := C.CString("password")
-	*password = pwd
-	*password_len = 8
+
+	key, err := keydb.DecodeString(config.KeyHandle + " " + config.KeyHash)
+	if err != nil {
+		log.Printf("Load keys failed: %v", err)
+		return -C.EINVAL
+	}
+	keys := keydb.AuthorisedKeys{key}
+
+	app := u2fhost.NewClient(u2fFacet)
+	c := make(chan string)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	res, err := app.CheckAuthenticate(ctx, keys.KeyHandlers())
+	if err != nil {
+		C.crypt_log(cd, C.CRYPT_LOG_NORMAL, C.CString(fmt.Sprintf("CheckAuthenticate failed:%v\n", err)))
+		return -C.ENOANO
+	}
+	if !res {
+		return -C.ENOANO
+	}
+
+	authfunc := func() {
+		if len(keys) == 0 {
+			c <- ""
+			return
+		}
+		pwd, err := u2fluks.Authorize(ctx, app, keys)
+		if err != nil {
+			C.crypt_log(cd, C.CRYPT_LOG_NORMAL, C.CString(fmt.Sprintf("Authorize failed:%v\n", err)))
+			c <- ""
+		} else {
+			C.crypt_log(cd, C.CRYPT_LOG_NORMAL, C.CString("Touch Registered\n"))
+			c <- pwd
+		}
+	}
+	go authfunc()
+	pwd := <-c
+	if pwd == "" {
+		return -C.ENOKEY
+	}
+	*password = C.CString(pwd)
+	*password_len = C.size_t(len(pwd))
 	return 0
 }
 
 //export cryptsetup_token_dump
 func cryptsetup_token_dump(cd *C.struct_crypt_device, cjson *C.char) {
-	C.crypt_log(cd, C.CRYPT_LOG_NORMAL, C.CString("Hello U2F\n"))
-	//C.crypt_log(cd, C.CRYPT_LOG_DEBUG_JSON, C.CString("WOO\n"))
+	var config TokenConfig
+	if err := json.Unmarshal([]byte(C.GoString(cjson)), &config); err != nil {
+		C.crypt_log(cd, C.CRYPT_LOG_NORMAL, C.CString(fmt.Sprintf("Invalid JSON config:%v\n", err)))
+		return
+	}
+	for s := range config.KeySlots {
+		C.crypt_log(cd, C.CRYPT_LOG_NORMAL, C.CString(fmt.Sprintf("Unlocks slot:%v\n", s)))
+	}
+	if config.IDHandle == "" {
+		C.crypt_log(cd, C.CRYPT_LOG_NORMAL, C.CString("No PIN/Passphrase requirement - or undeclared\n"))
+	} else {
+		C.crypt_log(cd, C.CRYPT_LOG_NORMAL, C.CString("PIN/Passphrase required\n"))
+	}
 }
 
 //export cryptsetup_token_validate
 func cryptsetup_token_validate(cd *C.struct_crypt_device, cjson *C.char) C.int {
-	fmt.Println("VALIDATE")
+	C.crypt_log(cd, C.CRYPT_LOG_NORMAL, C.CString("VALIDATE\n"))
+	var config TokenConfig
+	if err := json.Unmarshal([]byte(C.GoString(cjson)), &config); err != nil {
+		fmt.Printf("err:%v", err)
+		return -C.EINVAL
+	}
+	_, err := keydb.DecodeString(config.KeyHandle + " " + config.KeyHash)
+	if err != nil {
+		fmt.Printf("err:%v", err)
+		return -C.EINVAL
+	}
+	if config.IDHandle != "" {
+		_, err := keydb.DecodeString(config.IDHandle + " " + config.KeyHash)
+		if err != nil {
+			fmt.Printf("err:%v", err)
+			return -C.EINVAL
+		}
+	}
+
 	return 0
 }
 
 func main() {
-	fmt.Println("u2f-token")
 }
